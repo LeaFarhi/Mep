5baa0c8872d5594cd3e1111eb7af176aff62b8f7
LeaFarhi <lea.farhi@gmail.com>
2016-03-31 15:17:34 +0200

    Added files via upload

---
+++ InteractionNPC.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionNPC : MonoBehaviour
+{
+    public GUIText ChatNPC;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "maid")
+        {
+            SetChatNPC();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "maid")
+        {
+            ChatNPC.text = "";
+        }
+    }
+
+    void SetChatNPC()
+    {
+        ChatNPC.text = "Bienvenu dans notre hotel";
+    }
+}
